00bfd7db18cb7f3e29e1bb798cabd2f6555facd4
thatguytahd <todd.baker90@gmail.com>
2021-02-18 14:35:12 -0500

    Working on initial xhr request

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const updatePlayerResult = (
-    player {
-        id,
-        firstName,
-        lastName,
-        playerStats{careerStats{passingStats{attempts,completeions},rushingStats{attpempts,yardsPerAttempt}}}
-    }
-) => {
-    console.log(player);
-};
-
+const playerOutput = () => {
+    console.log(this.responseText);
+}
 
 const requestPlayerByName = (firstName, lastName) => {
     const baseURL = 'https://api.nfl.com/v1/persons?fs={id,firstName,lastName,playerStats{careerStats{passingStats{attempts,completeions},rushingStats{attpempts,yardsPerAttempt}}}}&s='
     const baseQuery = `{"$query":{"firstName" : "${firstName}","lastName" : "${lastName}"}})`
-    const xhr = new XMLHttpRequest;
-    xhr.onreadystatechange = (r) => {
-        updatePlayerResut(r);
-    };
-    xhr.open('GET', `${baseURL}+${baseQuery}`);
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', playerOutput);
+    xhr.open('GET', `${baseURL}${baseQuery}`);
     xhr.send();
 }
+
+requestPlayerByName('Tom', 'Brady');
